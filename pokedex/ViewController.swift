b538bb3eed1a96c15408cf3e456ac7ed3e612ff6
Vishnu Shankar <nunu@openvpn-client.oak-corp.jiff.com>
2017-08-14 12:13:48 -0700

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Vishnu Shankar on 8/14/17.
+//  Copyright © 2017 Vishnu Shankar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
